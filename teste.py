44517d165d41e645f51a3447e8ed0a039763d980
NunonCunha <nunoncunha@gmail.com>
2021-11-20 17:06:11 +0000

    estrita de novo ficheiro

---
+++ teste.py
@@ -0,0 +1,19 @@
+text = open("text.txt", "r")
+
+d = dict()
+
+for line in text:
+
+    line = line.strip()
+
+    line = line.lower()    
+
+    words = line.split(" ")
+
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+
+print(d)
