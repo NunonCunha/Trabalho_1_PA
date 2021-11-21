fb92dc9dfe96bbaf6619ab417f0134959109250d
NunonCunha <nunoncunha@gmail.com>
2021-11-21 14:18:26 +0000

    versão final, ajustar menus e saídas

--- ocorrencias.py
+++ ocorrencias.py
@@ -7,8 +7,8 @@ Alguns exemplares podem ser baixados gratuitamente do Projeto Gutenberg: https:/
 
 '''
 
-#DONE:1) O programa deve obter texto de um ficheiro;
-    #DONE:1.1 O nome do ficheiro deve ser dado pelo usuário.
+#TODO:1) O programa deve obter texto de um ficheiro;
+    #TODO:1.1 O nome do ficheiro deve ser dado pelo usuário.
 #TODO:2) O programa deve separar as palavras de modo a remover elementos de pontuação (pontos, vírgulas, exclamações, interrogações, travessões, ...);
     #TODO:2.1 A separação das palavras não deve ser case-sensitive (exemplo: 'casa' e 'Casa' devem ser tratadas como iguais).
     #TODO:2.2 Não há necessidade de separar radicais ou remover prefixos ou sufixos das palavras (exemplo: 'casa' e 'casas' pode ser consideradas como palavras diferentes).
@@ -36,50 +36,100 @@ https://www.gutenberg.org/cache/epub/4078/pg4078.txt
 #import para utilizar a função encoding para utilização de caracteres especiais
 import codecs
 
+#TODO: 1.1
 path = input("\nIntroduza o caminho e o nome do ficheiro (exemplo: c:\\teste.txt):\n")
 path = 'text.txt'
 
 # o encoding permite ler texto com caracteres especiais
 file = open(f"{path}", 'r',encoding='utf-8')
+
+#TODO: 1
 #leitura do ficheiro para uma lista
-texto = file.read().lower()
+texto = file.read()
+
+#Cria uma lista com caracteres
+pont_list_chr=[]
+
+def chr_list(begin, end):
+
+    #lê um inteiro e converte em caracter, e adiciona á lista
+    for i in range(begin, end):
+        pont_list_chr.append(chr(i))
+
+chr_list(33,48)
+chr_list(58,65)
+chr_list(91,97)
+chr_list(123,127)
+
+#função para remover todos os caracteres especiais
+def remove_char(text):
+    
+    for char in pont_list_chr:
+            text = text.replace(char, '')
+    return text
+
+texto = remove_char(texto)
+
+def convert_list(text):
+
+    for line in text:
+        line = line.strip()
+    lista = list(text.split())
+
+    return lista
+
+lista_final = convert_list(texto)
+
+# print(lista_final)
 
 
-#TODO:1
-#função para obter o texto do ficheiro text.txt
-def get_text():
-    pass
+def get_words_count(lista):
+    dic = {}
+    counter = 0
+    for word in lista:
+        if word in dic:
+            dic[word] = dic[word] + 1
+        else:
+            dic[word] = 1
+    
+    return dic
+
+dicionario = get_words_count(lista_final)
+
+# print(dicionario)
+
+#TODO:4
+#função para organizar o dicionário e faz print
+def order_score(dic):
+
+    '''ordenação do dicionário com função lambda em que a comparação é feita com o valor x[1]
+    utilizamos o reverse=True para alterar a ordem do sorted(),
+    esta foi a unica forma que encontrei para fazer o sort do dicionário
+    referencia para a função lambda. (https://towardsdatascience.com/two-simple-method-to-sort-a-python-dictionary-a7907c266dba)'''
+    dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+
+    # ciclo para percorrer o dicionario, mostra a palavras e o valor, das 20 mais utilizadas
+    for i in dic[:20]:
+	    print(i[0], i[1])
+
+         
 
 
-def save_text(text):
+def save_text(dic):
 
     new_file = open("newTextFile.txt","w")   
 
-    new_file.write(text)   
+    dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+
+    # ciclo para percorrer o dicionario, mostra a palavras e o valor, das 20 mais utilizadas
+    for i in dic[:20]:
+	    new_file.write(i[0], i[1])
+
+    # new_file.write(text) 
 
 
-def get_words_count():
-
-    pass
-  
-
-#TODO:2
-def delete_caracteres():
-    
-    pass
-
-#TODO:3
-def result_file():
-    pass
-
-#TODO:4
-def result_print():
-    pass
-
-
-
-save_text(texto)
-
+save_text(dicionario)
+order_score(dicionario)  
 
 #Fecha a ligação ao ficheiro
 file.close()
