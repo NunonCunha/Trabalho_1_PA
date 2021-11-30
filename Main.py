01f2614a9dd7dc648885b8a19bd7d69090b7fda8
NunonCunha <nunoncunha@gmail.com>
2021-11-30 13:05:23 +0000

    Melhoria, introdução do lugar ocupado pela palavra

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import codecs
 #Biblioteca para trabalhar com datas e horas
 import datetime
 
+
 print("***********************************")
 print("* Trabalho 1 Programação Avançada *")
 print("* Nome: Nuno Cunha                *")
@@ -39,14 +40,14 @@ elif option == '6':
     #Função que para a execução do programa
     exit()        
 else:
-    print("\nOpção inválida! O programa vai terminar")
-    exit()
+    print("\nOpção inválida! O programa vai terminar")        
+        
 
-#Abertura do ficheiro
+#Abertura do ficheiro, o bloco Try permite que o utilizador receba uma mensagem personalizada caso não seja possivel abrir o ficheiro
 try:
     file = open(f"{path}", 'r',encoding='utf-8')
 except:
-    print("\nErro ao abrir o ficheiro. O programa vai terminar")
+    print("\nErro ao abrir o ficheiro, O programa vai terminar")
     exit()
 
 #leitura do ficheiro para uma lista
@@ -70,27 +71,30 @@ chr_list(91,97)
 chr_list(123,127)
 
 #Função para remover todos os caracteres especiais
-def remove_char(text):    
+def remove_char(text):   
+
     for char in pont_list_chr:
             text = text.replace(char, '')
-
     return text
 
+#Atribui a string a variavel texto mas sem os caracteres especiais
 texto = remove_char(texto)
 
 #Função para converter a String sem os caracteres especiais em lista
 #A função strip() remove os espaços no inicio e fim da string
 def convert_list(text):
+
     for line in text:
         line = line.strip()
     lista = list(text.split())
-
     return lista
 
+#Cria uma lista de palavras separadas por espaço
 lista_final = convert_list(texto)
 
 #Função para criar um dicionario com os pares de palavras e a sua ocorrencia no texto lido
 def get_words_count(lista):
+
     dic = {}
     counter = 0
     for word in lista:
@@ -101,6 +105,7 @@ def get_words_count(lista):
     
     return dic
 
+#Cria um novo dicionário com a contagem das palavras
 dicionario = get_words_count(lista_final)
 
 #Função para organizar o dicionário e fazer print ordenado
@@ -109,27 +114,39 @@ def order_score(dic):
     utilizamos o reverse=True para alterar a ordem do sorted()
     referencia para a função lambda. (https://docs.python.org/3/reference/expressions.html#lambda) 
     (https://towardsdatascience.com/two-simple-method-to-sort-a-python-dictionary-a7907c266dba)'''
+
     dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
 
     # Ciclo para percorrer o dicionario, mostra a palavras e o valor, das 20 mais utilizadas
-    for i in dic[:20]:
-	    print(i[0], i[1])
-         
+    lugar = 1
+    while lugar < 20:
+        for i in dic[:20]:        
+            print(lugar,"ª -",i[0], i[1])
+            lugar += 1    
+
+
 #Função para gravar um ficheiro com o resultado das ocorrências
 def save_text(dic):
+
     date = datetime.date.today()
     new_text_file = f"Output\\ocorrencias_{str(date)}.txt"
     #Permite controlar se a execução foi realizada com sucesso ou não, caso não seja possivel abrir o ficheiro é devolvida uma mensagem
     try:
         new_file = open(f"{new_text_file}","w", encoding='utf-8')   
         dic = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-        for i in dic:
-            texto = str(i[0]) + "-" + str(i[1]) + "\n"
-            new_file.writelines(texto)
+        lugar = 1
+
+        #Percorre o dicionário de palavras e guarda no ficheiro por ordem
+        while lugar < 20:
+            for i in dic:
+                #Variavel para guardar no ficheiro a string com toda a informação que precisamos, lugar, palavra e nº de ocorrência
+                texto = str(lugar)+ " ª - " + str(i[0]) + "-" + str(i[1]) + "\n"
+                new_file.writelines(texto)
+                lugar += 1
         new_file.close()
-        print("\nFicheiro Guardado com exito. Verifique a lista completa em {}\n1".format(new_text_file))
+        print("\nFicheiro Guardado com exito. Verifique a lista completa em {}\n".format(new_text_file))
     except:
-        print("\nErro ao Gravar o ficheiro")
+        print("\nErro ao Gravar o ficheiro, o programa vai terminar.")
         exit()
     
 
--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Trabalho1_PA
-Ocorrências de palavras em ficheiro de texto
-
 -Trabalho prático 1 da disciplina de programação avançada do 3º ano de G.S.C
 
 -Programa que lê o numero de ocorrencias de palavras num determinado texto, o utilizador pode escolher entre textos pré-definidos ou introduzir o caminho para o seu proprio ficheiro
+
+
+Trabalho 1
+Regressar a: Semana 7 (15/11...
+O objetivo deste trabalho é desenvolver um programa em Python 3 que apresente dados estatísticos de número de ocorrência de palavras para um dado texto proveniente de um ficheiro. O texto pode ser um livro e, a partir deste programa, poderia ser analisado de acordo com a ocorrência de palavras (por exemplo, gerar nuvem de palavras, calcular a similaridade de textos, classificar a autoria de uma parte de texto, ...). Para testar, utilize livros no formato txt. Alguns exemplares podem ser baixados gratuitamente do Projeto Gutenberg: https://www.gutenberg.org/ebooks/.
+
+Os requisitos deste trabalho são:
+    1) O programa deve obter texto de um ficheiro;
+        - O nome do ficheiro deve ser dado pelo usuário.
+    2) O programa deve separar as palavras de modo a remover elementos de pontuação (pontos, vírgulas, exclamações, interrogações, travessões, ...);
+        - A separação das palavras não deve ser case-sensitive (exemplo: 'casa' e 'Casa' devem ser tratadas como iguais).
+        - Não há necessidade de separar radicais ou remover prefixos ou sufixos das palavras (exemplo: 'casa' e 'casas' pode ser consideradas como palavras diferentes).
+        - Algumas facilidades podem ser assumidas, desde que devidamente indicadas por comentário no código ou alguma outra documentação do trabalho. Por exemplo, o(a) aluno(a) pode manter o texto de identificação do projeto no início e fim do ficheiro, mesmo que este texto não faça parte do texto do autor (assume-se, a princípio, que isto não trará graves problemas para a contagem de palavras de modo geral).
+    3) Como saída do programa, deve ser gerado um novo ficheiro contendo cada palavra e seu respectivo número de ocorrências (ou percentual de ocorrência).
+    4) Uma outra saída esperada do programa é a apresentação (print no console) das 20 palavras mais utilizadas no texto.
+    5) As partes importantes do código devem ser comentadas (descrever o que está sendo feito).
+    6) O programa deve ser organizado em módulos (funções e procedimentos).
+
+Observações:
+
+Haverá um decréscimo na nota (20%) para cada dia ou fração de atraso.
+Alguns exemplares de livros em Português:
+https://www.gutenberg.org/cache/epub/34719/pg34719.txt
+https://www.gutenberg.org/files/62624/62624-0.txt
+https://www.gutenberg.org/cache/epub/24401/pg24401.txt
+https://www.gutenberg.org/cache/epub/54829/pg54829.txt
+Alguns exemplares de livros em Inglês:
+https://www.gutenberg.org/files/11/11-0.txt
+https://www.gutenberg.org/files/1661/1661-0.txt
+https://www.gutenberg.org/cache/epub/4078/pg4078.txt
